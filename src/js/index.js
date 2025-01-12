558823e68c261529f1e1c6281d9e28c7df7b6f24
uiuxkimabe <uiuxkimabe@gmail.com>
2025-01-12 13:03:33 +0700

    Section Navbar

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,12 @@
-/*? no js js needed from me */
+// hamBtn
+const hamBtn = document.querySelector('nav .hamBtn .menuBtn')
+const menu = document.querySelector('nav .menu')
+hamBtn.addEventListener('click', () => {
+    menu.classList.toggle('active')
+})
+
+document.addEventListener('click', (e) => {
+    if (!menu.contains(e.target) && !hamBtn.contains(e.target)) {
+        menu.classList.remove('active')
+    }
+})
