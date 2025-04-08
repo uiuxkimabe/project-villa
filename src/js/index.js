1b2af989fd10304a87d6421ff77a184bf63f75ce
uiuxkimabe <uiuxkimabe@gmail.com>
2025-04-08 22:49:42 +0700

    buat class dan function untuk menambahkan villa sesuai dengan nama, path folder, dan harga wd dan we tersendiri di js

--- src/js/index.js
+++ src/js/index.js
@@ -20,3 +20,46 @@ window.addEventListener('scroll',()=> {
         navbar.classList.remove('active')
     }
 })
+
+// Display Villa 
+class CardVilla {
+    constructor(villaName, pathFolder, priceWd, priceWe) {
+        this.villa = villaName;
+        this.path = pathFolder;
+        this.extentionImg = ['jpg', 'png', 'jpeg']
+        this.priceWd = priceWd;
+        this.priceWe = priceWe;
+    }
+    // Function Display Villa
+    displayVilla() {
+        const parentCard = document.querySelector('#cardVilla .row')
+        const createCard = document.createElement('a')
+        const priceHighlight = `${this.priceWd.toLocaleString('id-ID')} \- ${this.priceWe.toLocaleString('id-ID')} / Malam`
+        createCard.classList.add('card')
+        createCard.setAttribute('href', `${this.path}.html`)
+        createCard.setAttribute('target', '_blank')
+        createCard.innerHTML = `
+            <div class="card-header">
+                <img src="./src/img/${this.path}/1.${this.extentionImg[0]||this.extentionImg[1]||this.extentionImg[2]}" alt="${this.path}">
+            </div>
+            <div class="card-body">
+                <h3>${this.villa}</h3>
+                <p>Rp. ${priceHighlight}</p>
+            </div>
+        `
+        parentCard.appendChild(createCard)
+    }
+}
+
+// Villa Bambu
+const villaBambu = new CardVilla('Villa Bambu', 'villa-bambu', 2500000,3000000)
+villaBambu.displayVilla()
+// Villa gunung geulis
+const villaGunungGelis = new CardVilla('Villa Gunung Geulis', 'villa-gunung-gelis', 1500000,3000000)
+villaGunungGelis.displayVilla()
+// Villa Puncak
+const villaPuncak = new CardVilla('Villa Puncak', 'villa-puncak', 2000000,2500000)
+villaPuncak.displayVilla()
+// Villa Puri Indah
+const villaPuriIndah = new CardVilla('Villa Puri Indah', 'villa-puri-indah', 1500000,2000000)
+villaPuriIndah.displayVilla()
