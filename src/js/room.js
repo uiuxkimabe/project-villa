b0d4185757eb8d1840fdaf8597741c0722b12669
uiuxkimabe <uiuxkimabe@gmail.com>
2025-05-28 19:58:45 +0700

    add footer and class Profile Villa for room

--- src/js/room.js
+++ src/js/room.js
@@ -1,1 +1,22 @@
 // harga kamar
+class ProfileVilla {
+   constructor(name, rateWd, rateWe) {
+      this.villaName = name;
+      this.rateWd = rateWd;
+      this.rateWe = rateWe;
+   }
+
+   callVilla() {
+      const villaName = document.querySelector('.villaName');
+      const villaRate = document.querySelector('.villaPrice');
+      villaName.innerHTML = this.villaName;
+      villaName.style.fontWeight = 600;
+      villaRate.style.fontWeight = 600;
+      if (new Date().getDay() === 0 || new Date().getDay() === 6) {
+         villaRate.innerHTML = `Rp. ${this.rateWe.toLocaleString('id-ID')},- / malam`;
+      } else {
+         villaRate.innerHTML = `Rp. ${this.rateWd.toLocaleString('id-ID')},- / malam`;
+      }
+   }   
+}
+
